7b7a7b737da7073b25109f15d838463c06e00293
Ab-Daniel <31863068+Ab-Daniel@users.noreply.github.com>
2018-09-05 10:23:57 -0700

    Create SlopeTest.java

---
+++ SlopeTest.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class SlopeTest 
+{
+	
+	public static void main(String[] args) 
+	{
+		
+		JFrame frame = new JFrame();
+		frame.setSize(400, 450);
+		frame.setTitle("Slope of a Line");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+	
+}
